41c5b86c58f9cae8361dfe10fff671218a9ec240
Sriparno Roy <128922682+SriparnoRoy@users.noreply.github.com>
2023-04-27 20:06:27 +0530

    Update Sum of First N Natural Numbers in C++.cpp

--- Sum of First N Natural Numbers in C++.cpp
+++ Sum of First N Natural Numbers in C++.cpp
@@ -1,6 +1,8 @@
-// find the sum of first N natural numbers 
+// Find The Sum Of First N Natural Numbers 
+// Using --> For Loop
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int num, i, sum = 0;
@@ -11,5 +13,24 @@ int main()
         sum = sum + i;
     }
     cout << "sum is: " << sum;
+    return 0;
+}
+
+// Using--> While Loop
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, i = 1, sum = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (i <= num) {
+        sum += i;
+        i++;
+    }
+
+    cout << "Sum is: " << sum;
     return 0;
 }
